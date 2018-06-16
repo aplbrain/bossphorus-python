9536e749c9c4c7c29460e01e3bb204cb7f46dee9
Miller Wilt <miller.wilt@gmail.com>
2018-06-16 19:34:31 -0400

    Move concrete imports into create function

--- bossphorus/storagemanager/__init__.py
+++ bossphorus/storagemanager/__init__.py
@@ -1,6 +1,6 @@
-from ._FilesystemStorageManager import FilesystemStorageManager
 from .StorageManager import StorageManager
 
-
 def create(storage_path: str, block_size: [int, int, int]) -> StorageManager:
+    from ._FilesystemStorageManager import FilesystemStorageManager
+
     return FilesystemStorageManager(storage_path, block_size)
