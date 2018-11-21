86e8911dd66c34e92266f1310457e4a5f4edd8e0
Miller Wilt <miller.wilt@gmail.com>
2018-11-21 18:08:23 -0500

    Begin marmara zeromq-based server development

--- marmara/marmara/__init__.py
+++ marmara/marmara/__init__.py
@@ -1,5 +1,6 @@
 from . import coordinate_frame
 from . import engine
+from .marmara import Marmara
 from . import version
 
 __version__ = version.__version__
---
+++ marmara/marmara/marmara.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+import zmq
+
+class Marmara:
+    def __init__(self) -> None:
+        pass
